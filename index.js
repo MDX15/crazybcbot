4925ad818475cd21030738139bff502060ebc3f7
MDX15 <45093539+MDX15@users.noreply.github.com>
2018-12-25 21:33:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ m.sendMessage(args)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("for example .bc",{type: 'STREAMING'});
+          client.user.setActivity("For example .bc",{type: 'STREAMING'});
   
   });
 
